1bd53f067977a1bfe542590566fd764d9ec9691e
Suryansh Sharma <suryansh@memorres.com>
2025-03-13 09:30:00 +0530

    script.js

--- script.js
+++ script.js
@@ -5,14 +5,17 @@ document.addEventListener("DOMContentLoaded", function() {
 async function fetchSongData() {
     let songUrl = document.getElementById("searchThisSong").value;
 
-    try {
-        let response = await fetch(`https://api.song.link/v1-alpha.1/links?url=${encodeURIComponent(songUrl)}`);
-        let data = await response.json();
-        
-        let links = data.linksByPlatform;
+    if (!songUrl) {
+        document.getElementById("title").innerHTML = "No song URL found.";
+        return;
+    }
 
-        let songList = "<ul> Available on: ";
-        
+    try {
+        let response = await fetch(`https://api.song.link/v1-alpha.1/links?url=${songUrl}`);
+        let data = await response.json();
+        let links = data.linksByPlatform;
+        let songList = "Available on: <ul>";
+
         for (let platform in links) {
             songList += `<li><a href="${links[platform].url}" target="_blank" >${platform}</a></li>`;
         }
@@ -20,11 +23,14 @@ async function fetchSongData() {
         songList += "</ul>";
 
         let entityUniqueId = Object.keys(data.entitiesByUniqueId)[0];
+
+        if (!entityUniqueId) {
+            document.getElementById("title").innerHTML = "No title found for the song!";
+        }
+
         document.getElementById("title").innerHTML = `Song Title: ${data.entitiesByUniqueId[entityUniqueId].title}`;
         document.getElementById("results").innerHTML = songList;
-    }
-
-    catch (error) {
+    } catch (error) {
         console.error("Error fetching song data:", error.message);
     }
 }
